8d66af6ff6fc717ecc319329b384a86845c4c236
Grant Braught <braught@dickinson.edu>
2023-12-10 16:25:55 -0500

    Update startup.bash

--- startup.bash
+++ startup.bash
@@ -5,5 +5,16 @@ gp ports await 5901 > /dev/null
 gp ports await 6901 > /dev/null
 echo "Started."
 
-echo "The SSH URL is:"
-gp ssh | cut -f2 -d' '
+echo "Connect to the noVNC server at:"
+gh url 6901
+echo ""
+echo "OR"
+echo ""
+
+echo "Do the following on our machine to connect via VNC:"
+SSH_URL=$(gp ssh | cut -f2 -d' ')
+echo "  Run the command:"
+echo "    ssh -L 5901:localhost:5901 $SSH_URL"
+echo "  Connect your VNC Client to:"
+echo "    localhost:5901"
+echo ""
